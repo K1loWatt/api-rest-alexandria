45434ab3a684d458fa18377d86e70ad74d36233e
JaviMunir <moubayyed47@hotmail.com>
2025-02-08 11:21:28 +0100

    🔧 Settings class added, intended to be used, for now direct os vars are being passing to perform experiments

--- alexandria/app/settings.py
+++ alexandria/app/settings.py
@@ -2,22 +2,19 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-
     db_host: str = "localhost"
     db_port: int = 5432
     db_name: str = "myapp"
     app_name: str = "MyApp"
     debug: bool = False
-    version: str = "1.0.0" #TODO get this version from pyproject.toml
+    version: str = "1.0.0"
 
     class Config:
-        env_file = ".env"     
+        env_file = ".env"
 
-    @property
     def db_uri(self, username: str, password: str) -> str:
         return (
-            f"postgresql+asyncpg://{username}:"
-            f"{password}@{self.db_domain}:{self.db_port}"
-            f"/{self.db_name}"
+            f"postgresql://{username}:{password}@"
+            f"{self.db_host}:{self.db_port}/"
+            f"{self.db_name}"
         )
-
