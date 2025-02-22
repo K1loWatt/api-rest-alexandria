e27c81cd0a245bb86a6cf14202ea5b6ce300a8f5
JaviMunir <moubayyed47@hotmail.com>
2025-02-22 11:36:27 +0100

    ✨ add get_book endpoint with CQRS

--- alexandria/app/router.py
+++ alexandria/app/router.py
@@ -1,44 +1,14 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from typing import List
+from dependency import UOW, get_uow
 router = APIRouter()
-
-# here it goes the ReqBookCreation model
-# here it goes the ReqBookUpdate model
-# here it goes the RespBook model
-
-
-@router.get("/books/")
-def add_book(book):
-    # call to services
-    return {"test": "test"}
-
-
-"""
-@router.get("/books/")
-def list_books(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    books = crud.get_books(db, skip=skip, limit=limit)
+    
+async def get_books(uow):
+    with uow:
+        books = uow.repository.list()
     return books
 
-@router.get("/books/{book_id}")
-def get_book(book_id: int, db: Session = Depends(get_db)):
-    db_book = crud.get_book(db, book_id=book_id)
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return db_book
-
-@router.put("/books/{book_id}")
-def update_book(book_id: int, book: schemas.BookUpdate, db: Session = Depends(get_db)):
-    db_book = crud.get_book(db, book_id=book_id)
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return crud.update_book(db=db, book=book, book_id=book_id)
-
-@router.delete("/books/{book_id}", response_model=schemas.Book)
-def delete_book(book_id: int, db: Session = Depends(get_db)):
-    db_book = crud.get_book(db, book_id=book_id)
-    if db_book is None:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return crud.delete_book(db=db, book_id=book_id)
-
-
-"""
+@router.get("/books")
+def get_books(uow: UOW = Depends(get_uow)):
+    return  get_books(uow)
