b977463e1c20cfb0efdaaddbff027678270bf8a1
JaviMunir <moubayyed47@hotmail.com>
2025-02-26 18:12:21 +0100

    ✨ add app init procedure

--- alexandria/app/app.py
+++ alexandria/app/app.py
@@ -1,31 +1,34 @@
 from contextlib import asynccontextmanager
 from typing import AsyncGenerator
-
+import asyncio
 from fastapi import FastAPI
 
 from .router import router
 from .settings import Settings
-
-# async def db_engine_creation():
-#    return #engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 
 
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    # aqui todo lo que va antes de la inicialización de la propia aplicaci'on
-    # aqui va la inicialización de la BBDD y se agrega al app.state
-    print("Starting up...")
-    yield
-    # aqui va el engine dispose
-    print("Shutting down...")
+def create_life_span_with_settings(
+    settings: Settings
+    ):
+    
+    @asynccontextmanager
+    async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:        
+        engine = await asyncio.to_thread(create_async_engine(settings.db_uri))
+        app.state.engine = engine
+        yield
+        await engine.dispose()
 
-    # funcion para el lifespan
-    # TODO entender diferencia entre async def con y sin asynccontextmanager
-
+    return lifespan
 
 def create_app(settings: Settings) -> FastAPI:
-    app = FastAPI(title=settings.app_name, version=settings.version, life_span=lifespan)
-
+    
+    lifespan = create_life_span_with_settings(settings)
+    app = FastAPI(
+        title=settings.app_name, 
+        version=settings.version, 
+        lifespan=lifespan
+        )
     app.include_router(router)
 
     return app
